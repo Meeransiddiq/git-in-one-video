f18513088014fca7297910874d2b83107b3cf5ca
Meeran Siddiqui <mee.sidd.2019@gmail.com>
2022-11-20 08:52:35 +0530

    Added multiply.java

--- multiply.java
+++ multiply.java
@@ -2,7 +2,7 @@ public class multiply {
     public static void main(String args[]){
          int a=10;
          int b=20;
-         System.out.println(a*b);
+         System.out.println("product of two numbers is"+(a*b));
         
     }
     
