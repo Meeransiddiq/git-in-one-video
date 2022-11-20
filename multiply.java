ee58a821eceed800a8dea97a90e7fb0b3d27b904
Meeran Siddiqui <mee.sidd.2019@gmail.com>
2022-11-20 11:17:27 +0530

    editing multiply.java file

--- multiply.java
+++ multiply.java
@@ -2,8 +2,8 @@ public class multiply {
     public static void main(String args[]){
          int a=10;
          int b=20;
-         System.out.println("product of two numbers "+(a*b));
-         
+         System.out.println("product of two number is  "+(a*b));
+
         
     }
     
